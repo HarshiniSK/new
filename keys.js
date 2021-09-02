edb808f14d1f8456ba6dbdc14aca1a0b76fb73d0
HarshiniSK <harshini28102@gmail.com>
2021-09-02 19:20:59 +0530

    change

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    iam_access_id:'AKIASCJ5BPQFTV2BJ7EW',
-    iam_secret:'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',
-};
+    iam_access_id:'AKIASCJ5BPQFUPAZDV6G',
+    iam_secret:'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',
+};  
